8f444e9fda77aebffdea7858e8a6161e26a402a8
Gopal Pawar <Gopal.Pawar@veritas.com>
2020-11-17 15:56:15 +0530

    using express.json for allowing json raw format request

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
 const express = require('express')
 const app = express()
 const router = require('./studentrouter')
-const bodyParser = require('body-parser')
+//const bodyParser = require('body-parser')
+
+app.use(express.json())
 
 // parse application/x-www-form-urlencoded
-app.use(bodyParser.urlencoded({ extended: false }))
+//app.use(bodyParser.urlencoded({ extended: false }))
 
 // parse application/json
-app.use(bodyParser.json())
+//app.use(bodyParser.json())
 
 app.use('/students', router)
 app.listen(5000, () => console.log('Server is running'))
